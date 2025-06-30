ec02841671043a5f2f20c1e5a7b5134c1c6f1d14
Ajeet <ajeet@arizona.edu>
2025-06-30 00:58:40 -0700

    Data Validation code completed

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig
 import sys
 
 if __name__ == "__main__":
     try:
-        trauning_pipeline_config = TrainingPipelineConfig()
-        data_ingestion_config = DataIngestionConfig(trauning_pipeline_config)
+        training_pipeline_config = TrainingPipelineConfig()
+        data_ingestion_config = DataIngestionConfig(training_pipeline_config)
         data_ingestion = DataIngestion(data_ingestion_config)
         logging.info(f"Data Ingestion started with config: {data_ingestion_config}")
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
         logging.info("MAIN-- Data Ingestion Completed successfully.")
         print(data_ingestion_artifact)
+
+
+
+        data_validation_config = DataValidationConfig(training_pipeline_config)
+        data_validation = DataValidation(data_ingestion_config,data_validation_config)
+        logging.info(f"Data Validation started with config: {data_validation_config}")
+        data_validation_artifact= data_validation.initiate_data_validation()
+        logging.info("MAIN-- Data Validation Completed successfully.")
+        print(data_validation_artifact)
+
+
     
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -11,10 +11,12 @@ TARGET_COLUMN = "Result"
 PIPELINE_NAME: str = "NetworkSecurity"
 ARTIFACT_DIR: str = "Artifacts"
 FILE_NAME: str = "phishingData.csv"
+
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 
 
+SCHEMA_FILE_PATH = os.path.join("networksecurity","data_schema", "schema.yaml")
 
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
@@ -26,3 +28,13 @@ DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
 
+
+"""
+Data Validation related constant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
