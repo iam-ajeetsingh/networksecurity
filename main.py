13770ea965d80352c1c7fad980a39200033bda0b
Ajeet <ajeet@arizona.edu>
2025-07-04 00:25:03 -0700

    Data Transformation Code added.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig, DataTransformationConfig
 import sys
 
 if __name__ == "__main__":
@@ -25,7 +26,13 @@ if __name__ == "__main__":
         print(data_validation_artifact)
 
 
-    
+        data_transformation_config = DataTransformationConfig(training_pipeline_config)
+        logging.info("Data Transformation started with config: {data_transformation_config}")
+        data_transformation = DataTransformation(data_validation_artifact, data_transformation_config)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        logging.info("MAIN-- Data Transformation Completed successfully.")
+        print(data_transformation_artifact)
+
     except Exception as e:
         raise NetworkSecurityException(e, sys)
     
